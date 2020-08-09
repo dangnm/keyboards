3faa87424a1f2858974ba77ef3787e1f2e48e792
Phong Nguyen <nhphong1406@gmail.com>
2020-08-09 18:39:06 +0700

    Update layers and readme

--- interdox/config.h
+++ interdox/config.h
@@ -17,3 +17,9 @@
 #define TAPPING_TERM    200
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
+// Always send Escape if Alt is pressed
+#define GRAVE_ESC_ALT_OVERRIDE
+// Always send Escape if Control is pressed
+#define GRAVE_ESC_CTRL_OVERRIDE
+// Always send Escape if GUI is pressed
+#define GRAVE_ESC_GUI_OVERRIDE
--- interdox/keymap.c
+++ interdox/keymap.c
@@ -4,6 +4,7 @@ extern keymap_config_t keymap_config;
 
 enum redox_layers {
     _BASE,
+    _PHONG,
     _ARROW,
     _EDGE,
     _SYMBOL,
@@ -21,6 +22,7 @@ enum redox_keycodes {
 
 // clang-format off
 #define BASE    DF(_BASE)
+#define PHONG   DF(_PHONG)
 #define ARROW   DF(_ARROW)
 #define EDGE    DF(_EDGE)
 #define LOWER   MO(_LOWER)
@@ -44,13 +46,21 @@ enum redox_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,                         KC_ESC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,                         KC_GESC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    MOU_SCL, GUI_QUO,
         KC_LSFT, CTL_Z,   KC_X,    KC_C,    KC_V,    KC_B,    LWR_PGU, KC_PGDN,      KC_HOME, RSE_END, KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_SLS, KC_RSFT,
         KC_GRV,  CAG_QUO, KC_LBRC, KC_RBRC,     KC_LGUI,      KC_SPC,  KC_BSPC,      KC_TAB,  KC_ENT,      KC_LALT,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
-    [_EDGE] = LAYOUT(
+    [_PHONG] = LAYOUT(
+        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,                         KC_GRV,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    MOU_SCL, GUI_QUO,
+        KC_LSFT, CTL_Z,   KC_X,    KC_C,    KC_V,    KC_B,    LWR_PGU, KC_PGDN,      KC_HOME, RSE_END, KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_SLS, KC_RSFT,
+        KC_GRV,  CAG_QUO, KC_LBRC, KC_RBRC,     KC_LGUI,      KC_SPC,  KC_BSPC,      KC_TAB,  KC_ENT,      KC_LALT,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+
+    [_ARROW] = LAYOUT(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,                         KC_GRV,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FN_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    MOU_SCL, GUI_QUO,
@@ -76,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN] = LAYOUT(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, _______, KC_UP,   _______, _______, _______, _______,                        _______, _______, _______, KC_UP,   _______, _______, KC_F12,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,                        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+        _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______,                        _______, KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_F12,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______,                        _______, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______,     _______,      _______, _______,      _______, _______,     _______,      _______, _______, _______, _______
     ),
@@ -107,10 +117,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_MOUSE] = LAYOUT(
-        RESET,   BASE,    ARROW,   EDGE,    _______, _______,                                          _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
+        RESET,   BASE,    PHONG,   ARROW,   EDGE,    _______,                                          _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
         _______, _______, KC_BTN3, KC_MS_U, KC_BTN2, _______, AG_NORM,                        AG_SWAP, _______, KC_BTN2, KC_WH_U, KC_BTN3, _______, _______,
         KC_CAPS, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_F14,                         KC_F15,  KC_BTN1, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,
-        _______, KC_VOLD, KC_VOLD, KC_MUTE, _______, _______, _______, _______,      _______, _______, AG_NORM, AG_SWAP, _______, _______, _______, _______,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______,      _______, _______, AG_NORM, AG_SWAP, _______, _______, _______, _______,
         EPRM,    _______, _______, _______,     _______,      KC_BTN1, KC_BTN2,      KC_BTN1, KC_BTN2,     _______,      KC_VOLU, KC_VOLD, _______, _______
     ),
 
