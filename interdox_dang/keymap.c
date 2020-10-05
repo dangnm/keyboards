383fba28a690c6c178994b72af85c6e36e245db9
Dang Nguyen <dang.nguyen@employmenthero.com>
2020-10-05 13:32:09 +0700

    capslock layer 1

--- interdox_dang/keymap.c
+++ interdox_dang/keymap.c
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_LAYOUT1] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_CAPS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_VOLU,                          TOGGL2,  KC_SLSH, KC_AMPR, KC_8,    KC_LPRN, KC_PAST, KC_F12,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_VOLD,                          _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS, KC_MFFD,
+        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_VOLU,                          TOGGL2,  KC_SLSH, KC_AMPR, KC_8,    KC_LPRN, KC_PAST, KC_F12,
+        KC_CAPS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_VOLD,                          _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS, KC_MFFD,
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_MSTP, _______,        _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_MRWD,
         _______, _______, _______, _______,     _______,      KC_BTN1, _______,        _______, KC_BTN2,    LAYOU1,        KC_0,    KC_DOT,  _______, TOGGL2
     ),
